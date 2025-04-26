8e712c814b8c80f1bbaa03d2e284740781e789b7
家炫 刘 <1928099560@qq.com>
2025-04-26 15:56:31 +0800

    使用系统提供的虚拟内存管理函数（Windows）

--- FixedMempool.hpp
+++ FixedMempool.hpp
@@ -2,65 +2,69 @@
 
 #include <iostream>
 #include <vector>
-using std::cout;
+#include <Windows.h>
+#include <memoryapi.h>
 using std::cin;
+using std::cout;
+
+
 
 template <class T>
 class FLMemPool
 {
 public:
-    T* New()
+    T *New()
     {
-        T* ret = nullptr;
-        if(freelist)
+        T *ret = nullptr;
+        if (freelist)
         {
-            //如果回收站有空间，直接将内存块从回收站中取出使用
-            ret = (T*)freelist;
-            freelist = *(void**)freelist;
+            // 如果回收站有空间，直接将内存块从回收站中取出使用
+            ret = (T *)freelist;
+            freelist = *(void **)freelist;
         }
         else
         {
-            if(leftMemLen < sizeof(T))
+            if (leftMemLen < sizeof(T))
             {
                 leftMemLen = 1024 * 128;
-                memory = (char*)malloc(leftMemLen);
-                if(!memory)
+                memory = (char *)VirtualAlloc(nullptr, 1024 * 128, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+                if (!memory)
                 {
                     throw std::bad_alloc();
                 }
-                //析构时释放
+                // 析构时释放
                 blocks.push_back(memory);
             }
-            ret = (T*)memory;
-            //保证可以存下单个地址长度以便建立链表
-            size_t obj_len = sizeof(T) > sizeof(void*)? sizeof(T): sizeof(void*);
+            ret = (T *)memory;
+            // 保证可以存下单个地址长度以便建立链表
+            size_t obj_len = sizeof(T) > sizeof(void *) ? sizeof(T) : sizeof(void *);
             memory += obj_len;
             leftMemLen -= obj_len;
         }
-        //定位new调用构造函数
-        new(ret)T;
+        // 定位new调用构造函数
+        new (ret) T;
         return ret;
     }
 
-    void Delete(T* obj)
+    void Delete(T *obj)
     {
         obj->~T();
-        //头插法将freelist的地址存放在obj首个地址长度当中
-        *(void**)obj = freelist;
+        // 头插法将freelist的地址存放在obj首个地址长度当中
+        *(void **)obj = freelist;
         freelist = obj;
     }
 
-    //最终释放内存
+    // 最终释放内存
     ~FLMemPool()
     {
         freelist = nullptr;
-        for(char* block: blocks)
-        free(block);
+        for (char *block : blocks)
+            VirtualFree(block, 0, MEM_RELEASE);
     }
 
 private:
-    char* memory = nullptr;
+    char *memory = nullptr;
     size_t leftMemLen = 0;
-    void* freelist = nullptr;
-    std:: vector<char*> blocks;
+    void *freelist = nullptr;
+    std::vector<char *> blocks;
 };
