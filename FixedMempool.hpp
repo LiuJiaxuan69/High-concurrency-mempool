fb3a6bf5fdc919693bd7cc8924fec27e85624ae8
家炫 刘 <1928099560@qq.com>
2025-04-26 15:24:04 +0800

    定长内存池

---
+++ FixedMempool.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::cin;
+
+template <class T>
+class FLMemPool
+{
+public:
+    T* New()
+    {
+        T* ret = nullptr;
+        if(freelist)
+        {
+            //如果回收站有空间，直接将内存块从回收站中取出使用
+            ret = (T*)freelist;
+            freelist = *(void**)freelist;
+        }
+        else
+        {
+            if(leftMemLen < sizeof(T))
+            {
+                leftMemLen = 1024 * 128;
+                memory = (char*)malloc(leftMemLen);
+                if(!memory)
+                {
+                    throw std::bad_alloc();
+                }
+                //析构时释放
+                blocks.push_back(memory);
+            }
+            ret = (T*)memory;
+            //保证可以存下单个地址长度以便建立链表
+            size_t obj_len = sizeof(T) > sizeof(void*)? sizeof(T): sizeof(void*);
+            memory += obj_len;
+            leftMemLen -= obj_len;
+        }
+        //定位new调用构造函数
+        new(ret)T;
+        return ret;
+    }
+
+    void Delete(T* obj)
+    {
+        obj->~T();
+        //头插法将freelist的地址存放在obj首个地址长度当中
+        *(void**)obj = freelist;
+        freelist = obj;
+    }
+
+    //最终释放内存
+    ~FLMemPool()
+    {
+        freelist = nullptr;
+        for(char* block: blocks)
+        free(block);
+    }
+
+private:
+    char* memory = nullptr;
+    size_t leftMemLen = 0;
+    void* freelist = nullptr;
+    std:: vector<char*> blocks;
+};
