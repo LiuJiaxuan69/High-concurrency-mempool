838919abcf0fd546baa6c1aacda99a71a48a3817
家炫 刘 <1928099560@qq.com>
2025-04-29 23:33:01 +0800

    完善CentralCache中向PageCache申请页面资源函数

--- CentralCache.cc
+++ CentralCache.cc
@@ -1,4 +1,5 @@
 #include "CentralCache.hpp"
+#include "PageCache.hpp"
 
 size_t CentralCache::FetchRangeObj(void *&start, void *&end, size_t batchNum, size_t byte)
 {
@@ -27,5 +28,40 @@ size_t CentralCache::FetchRangeObj(void *&start, void *&end, size_t batchNum, si
 }
 Span* CentralCache::GetOneSpan(SpanList &list, size_t byte)
 {
-    return nullptr;
+    Span *it = list.Begin();
+    //若找得到剩余的的Span则直接返回
+    while(it != list.End())
+    {
+        if(it != nullptr) return it;
+        it = it->next;
+    }
+
+    //先释放桶锁，别的线程可能正在释放资源
+    list.mtx.unlock();
+
+    //需要向上层PageCache申请Page进行分割
+    PageCache::GetInstance()->mtx.lock();
+    Span *span = PageCache::GetInstance()->NewSpan(SizeClass::NumMovePage(byte));
+    PageCache::GetInstance()->mtx.unlock();
+
+    //对页切片
+    char *start = (char*)(span->pageId << PAGE_SHIFT);
+    size_t size = span->n << PAGE_SHIFT;
+    char *end = start + size;
+
+    span->freelist = start;
+    start += byte;
+    void *tail = span->freelist;
+    while(start < end)
+    {
+        NextObj(tail) = start;
+        tail = start;
+        start += byte;
+    }
+
+    //回去之前上锁
+    list.mtx.lock();
+    
+    list.Insert(list.Begin(), span);
+    return span;
 }
