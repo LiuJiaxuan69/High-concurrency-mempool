ab009da93e00d5f5de524cc46a95d1acf4e5754a
家炫 刘 <1928099560@qq.com>
2025-05-01 16:50:46 +0800

    代码优化

--- Concurrent.hpp
+++ Concurrent.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "ThreadCache.hpp"
+#include <format>
+#include <syncstream>
 
 inline void* ConcurrentAlloc(size_t byte)
 {
@@ -8,8 +10,18 @@ inline void* ConcurrentAlloc(size_t byte)
     {
         thread_local_data_ = new ThreadCache();
     }
-    std::cout << std::this_thread::get_id() << ":" << thread_local_data_ << std::endl;
+    // auto output1 = std::this_thread::get_id();
+    // std::cout << std::format("{}:{}\n", output1, (void*)thread_local_data_);
+    // std::osyncstream(std::cout) << std::this_thread::get_id() << ":" << thread_local_data_ << std::endl;
     return thread_local_data_->Allocate(byte);
+}
+
+template<class T, class... Args>
+T* testAlloc(Args&&... args)
+{
+    T* ret = (T*)ConcurrentAlloc(sizeof(T));
+    new (ret) T(std::forward<Args>(args)...);
+    return ret;
 }
 
 inline void* ConcurrentFree(void *ptr)
--- PageCache.cc
+++ PageCache.cc
@@ -3,34 +3,43 @@
 Span *PageCache::NewSpan(size_t k)
 {
     assert(k >= 1 && k <= NPAGES);
-    //检查当前Span下有没有可用的PAGE
-    if(!_spanLists[k].Empty())
+    // 检查当前Span下有没有可用的PAGE
+    if (!_spanLists[k].Empty())
     {
         return _spanLists[k].PopFront();
     }
-    //否则去后面查看是否有非空Span可使用来分割
-    for(int i = k + 1; i <= NPAGES; ++i)
+    // 否则去后面查看是否有非空Span可使用来分割
+    for (int i = k + 1; i <= NPAGES; ++i)
     {
-        if(_spanLists[i].Empty()) continue;
-        //发现非空Span，可以将其分割位两份Span
+        if (_spanLists[i].Empty())
+            continue;
+        // 发现非空Span，可以将其分割位两份Span
         Span *span = _spanLists[i].PopFront();
         Span *kspan = new Span();
         kspan->pageId = span->pageId;
         kspan->n = k;
-        span->n -= k;
         span->pageId += k;
+        span->n -= k;
 
         _spanLists[i - k].PushFront(span);
         return kspan;
     }
 
-    //此时说明没有大块Span供以分割，此时需要向上层（系统）索要一块堆空间
+    // 此时说明没有大块Span供以分割，此时需要向上层（系统）索要一块堆空间
     void *ptr = VirtualAlloc(0, NPAGES << PAGE_SHIFT, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-    if(ptr == nullptr) throw std::bad_alloc();
-    //将该内存
+    if (ptr == nullptr)
+        throw std::bad_alloc();
+    // 将该内存分成两份
     Span *newSpan = new Span();
     newSpan->n = NPAGES;
     newSpan->pageId = (PAGE_ID)ptr >> PAGE_SHIFT;
-    _spanLists[NPAGES].PushFront(newSpan);
-    return NewSpan(k);
+
+    //直接从newSpan中切割
+    Span *kspan = new Span();
+    kspan->pageId = newSpan->pageId;
+    kspan->n = k;
+    newSpan->pageId += k;
+    newSpan->n -= k;
+    _spanLists[newSpan->n].PushFront(newSpan);
+    return kspan;
 }
